68dbad8aac14912b50121111659e3a8d5fe04785
andr68rus <ashtmb@gmail.com>
2018-03-25 16:48:22 +0300

    add WfdCommon to PRODUCT_BOOT_JARS

--- device.mk
+++ device.mk
@@ -297,7 +297,9 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     qcrilhook \
-    telephony-ext
+    telephony-ext \
+    tcmiface \
+    WfdCommon
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
